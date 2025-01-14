7d66999c819b6b19ccccb75a7b1fa9910d95c247
viniciusrabello <viniciusrabello@matematica.ufrj.br>
2025-01-14 16:09:33 -0300

    deleted staging_exercises.sql

--- models/staging/staging__exercises.sql
+++
@@ -1,8 +0,0 @@
-with source_exercises as (
-    select * from {{ source('gym_app_db', 'exercises') }}
-    where muscle_group = 'Ombros'
-), final as (
-    select * from source_exercises
-)
-
-select * from final
